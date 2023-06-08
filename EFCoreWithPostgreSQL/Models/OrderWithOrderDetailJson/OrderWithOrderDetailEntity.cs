c81b04f04ee8d77a38a5b7dfafed08a7336ae4c0
smitesh-maniya <123544692+smitesh-maniya@users.noreply.github.com>
2023-06-08 17:43:19 +0530

    change type double to float

--- EFCoreWithPostgreSQL/Models/OrderWithOrderDetailJson/OrderWithOrderDetailEntity.cs
+++ EFCoreWithPostgreSQL/Models/OrderWithOrderDetailJson/OrderWithOrderDetailEntity.cs
@@ -11,8 +11,8 @@ namespace EFCoreJsonApp.Models.OrderWithOrderDetail
     public class OrderDetailsJson
     {
         public string ItemName { get; set; }
-        public double Price { get; set; }
+        public float Price { get; set; }
         public int Quantity { get; set; }
-        public double Total { get; set; }
+        public float Total { get; set; }
     }
 }
